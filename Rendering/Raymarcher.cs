395c8052d318245749f4f6cc75dfb18aedf3293a
cyprus327 <jrcjr327@tutanota.com>
2023-06-26 17:34:31 -0400

    Major improvements to movement

--- Rendering/Raymarcher.cs
+++ Rendering/Raymarcher.cs
@@ -2,11 +2,14 @@
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 
 namespace Raymarching.Rendering;
 
 internal sealed class Raymarcher : IDisposable {
     public Raymarcher(int width, int height) {
+        viewport = new Vector2((float)width, (float)height);
+
         char c = Path.DirectorySeparatorChar;
         string fragShaderPath = @$"..{c}..{c}..{c}Rendering{c}Shaders{c}frag.glsl";
         string fragCode = ShaderReader.ReadToString(fragShaderPath);
@@ -47,8 +50,8 @@ internal sealed class Raymarcher : IDisposable {
 
         GL.UseProgram(Handle);
 
-        int viewportLocation = GL.GetUniformLocation(Handle, "uViewport");
-        GL.Uniform2(viewportLocation, (float)width, (float)height);
+        _viewportUniformLocation = GL.GetUniformLocation(Handle, "uViewport");
+        GL.Uniform2(_viewportUniformLocation, viewport);
 
         _timeUniformLocation = GL.GetUniformLocation(Handle, "uTime");
         _camPosUniformLocation = GL.GetUniformLocation(Handle, "uCamPos");
@@ -56,6 +59,9 @@ internal sealed class Raymarcher : IDisposable {
     }
 
     public int Handle { get; init; }
+
+    private readonly int _viewportUniformLocation;
+    private Vector2 viewport;
 
     private readonly int _timeUniformLocation;
     private float elapsedTime = 0f;
@@ -68,57 +74,69 @@ internal sealed class Raymarcher : IDisposable {
 
     private bool disposed = false;
 
-    public void HandleInput(float deltaTime, KeyboardState keyboardState, MouseState mouseState) {
+    public void OnResize(ResizeEventArgs e) {
+        viewport = new Vector2(e.Width, e.Height);
+    }
+
+    public void HandleInput(float deltaTime, KeyboardState keyboardState, MouseState mouseState, out CursorState cursorState) {
         elapsedTime += deltaTime;
 
-        // camera movement
+        float dx = 0f, dy = 0f;
+        if (mouseState.IsButtonDown(MouseButton.Button2)) {
+            dx = mouseState.Delta.X;
+            // ignore dy for now
+            dy = 0f;//mouseState.Delta.Y;
+            cursorState = CursorState.Grabbed;
+        } else {
+            cursorState = CursorState.Normal;
+        }
+
+        Vector3 dir = Vector3.Normalize(camPos - objPos);
+        Vector3 right = Vector3.Cross(dir, Vector3.UnitY);
+
+        const float SENS = 0.025f;
+        camPos -= right * dx * SENS;
+        camPos -= Vector3.Cross(dir, right) * dy * SENS;
+
         float speed = keyboardState.IsKeyDown(Keys.LeftShift) ? 20f : 5f;
-        if (keyboardState.IsKeyDown(Keys.Space)) {
-            camPos.Y += deltaTime * speed;
-        } else if (keyboardState.IsKeyDown(Keys.LeftControl)) {
-            camPos.Y -= deltaTime * speed;
-        } 
+        if (keyboardState.IsKeyDown(Keys.E)) {
+            objPos += Vector3.UnitY * deltaTime * speed;
+        } else if (keyboardState.IsKeyDown(Keys.Q)) {
+            objPos -= Vector3.UnitY * deltaTime * speed;
+        }
+
+        // TODO: make this (using startY) better by removing it
+        float startY = objPos.Y;
+
         if (keyboardState.IsKeyDown(Keys.W)) {
-            camPos.Z += deltaTime * speed;
+            objPos -= dir * deltaTime * speed;
         } else if (keyboardState.IsKeyDown(Keys.S)) {
-            camPos.Z -= deltaTime * speed;
-        } 
+            objPos += dir * deltaTime * speed;
+        }
         if (keyboardState.IsKeyDown(Keys.A)) {
-            camPos.X -= deltaTime * speed;
+            Vector3 delta = right * deltaTime * speed;
+            objPos -= delta;
+            camPos -= delta;
         } else if (keyboardState.IsKeyDown(Keys.D)) {
-            camPos.X += deltaTime * speed;
+            Vector3 delta = right * deltaTime * speed;
+            objPos += delta;
+            camPos += delta;
         }
+        objPos.Y = startY;
 
-        // object uniform movement
-        if (keyboardState.IsKeyDown(Keys.R)) {
-            objPos.Y -= deltaTime;
-        } else if (keyboardState.IsKeyDown(Keys.Y)) {
-            objPos.Y += deltaTime;
-        } else if (keyboardState.IsKeyDown(Keys.T)) {
-            objPos.Z += deltaTime;
-        } else if (keyboardState.IsKeyDown(Keys.F)) {
-            objPos.X -= deltaTime;
-        } else if (keyboardState.IsKeyDown(Keys.G)) {
-            objPos.Z -= deltaTime;
-        } else if (keyboardState.IsKeyDown(Keys.H)) {
-            objPos.X += deltaTime;
-        }
+        const float CAM_DIST = 6f;
+        camPos = objPos - Vector3.Normalize(objPos - camPos) * CAM_DIST;
 
-        GetMouseDelta(mouseState, out float dx, out float dy);
+        //const float MAX_HEIGHT = 5.5f;
+        //if (MathF.Abs(camPos.Y - objPos.Y) > MAX_HEIGHT) {
+        //    camPos.Y = camPos.Y < objPos.Y ? objPos.Y - MAX_HEIGHT : objPos.Y + MAX_HEIGHT;
+        //}
+        camPos.Y = objPos.Y + 4f;
 
+        GL.Uniform2(_viewportUniformLocation, viewport);
         GL.Uniform1(_timeUniformLocation, elapsedTime);
         GL.Uniform3(_camPosUniformLocation, camPos);
         GL.Uniform3(_objPosUniformLocation, objPos);
-    }
-
-    private void GetMouseDelta(MouseState state, out float dx, out float dy) {
-        if (state.IsButtonDown(MouseButton.Button1)) {
-            dx = state.Delta.X;
-            dy = state.Delta.Y;
-        } else {
-            dx = 0f;
-            dy = 0f;
-        }
     }
 
     ~Raymarcher() {
