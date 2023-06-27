2c418e8fd58b6bc60e9afc75de0ec542c8364180
cyprus327 <jrcjr327@tutanota.com>
2023-06-26 21:49:52 -0400

    Move structs to their own file

---
+++ Common/ObjStructs.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Raymarching.Common;
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct Vec3 {
+    public Vec3(float x, float y, float z) {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public float X;
+    public float Y;
+    public float Z;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal struct Sphere {
+    public Vec3 C;
+    public float R;
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 16)]
+internal struct Cube {
+    public Vec3 C;
+    private float padding1;
+    public Vec3 S;
+    private float padding2;
+}
